8012fd53e72dfea35a7eae51f9e39da989ede547
Anshika <anshikathakur596@gmail.com>
2023-06-29 18:57:00 +0530

    review section added

---
+++ javascript/report.js
@@ -0,0 +1,123 @@
+var head_1=document.getElementById("heading1");
+var box_1=document.getElementById("box1");
+var box_2=document.getElementById("box2");
+var box_3=document.getElementById("box3");
+var opt1 =document.getElementById("opt2");
+var opt2 =document.getElementById("opt3");
+var opt3 =document.getElementById("opt4");
+var opt4 =document.getElementById("opt5");
+var opt5 =document.getElementById("opt6");
+var opt6 =document.getElementById("opt7");
+
+
+
+function advertising(){
+    head_1.innerHTML=`<h1>Advertising <br>& Media</h1>
+    <p>CAGR of 5.6% <br> Market Value of $23.9 Trillion</p>`;
+
+    box_1.innerHTML=` <b>Global Platform Screen Doors (PSD) Market Size was USD 859.50 Million in 2022, this report covers Market growth, trend, opportunity and forecast 2023-2030</b>
+    <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+            153</span><span>Published: 39 days ago</span><span>Price : $ 3590</span></p>`;
+
+    box_2.innerHTML=`<b>Stearates Market Size is growing at CAGR of 4.81%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+    <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+            180</span><span>Published: 35 days ago</span><span>Price : $ 3590</span></p>`;
+
+    box_3.innerHTML=`<b>Metal Packaging Coatings Market Size is growing at CAGR of 3.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+    <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+            162</span><span>Published: 35 days ago</span><span>Price : $ 3590</span></p>`;
+}
+
+function automobile(){
+    head_1.innerHTML=`<h1>Automobile <br> Industry</h1>
+    <p>CAGR of 5.6% <br> Market Value of $23.9 Trillion</p>`;
+
+    box_1.innerHTML=`<b>Automotive Sensors Market by Sales Channel (OEM, Aftermarket)</b>
+    <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+            112</span><span>Published: 2 days ago</span><span>Price : $ 2999</span></p>`;
+    
+    box_2.innerHTML=`<b>Automotive Sensors Market by Sales Channel (OEM, Aftermarket)</b>
+    <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+            112</span><span>Published: 2 days ago</span><span>Price : $ 2999</span></p>`;
+
+    box_3.innerHTML=`<b>Automotive Sensors Market by Sales Channel (OEM, Aftermarket)</b>
+    <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+            112</span><span>Published: 2 days ago</span><span>Price : $ 2999</span></p>`;
+}
+
+function BeautyCare(){
+
+        head_1.innerHTML=`<h1>BeautyCare</h1>
+        <p>CAGR of 5.6% <br> Market Value of $23.9 Trillion</p>`;
+    
+        box_1.innerHTML=`<b>Disc Golf Market Size is growing at CAGR of 17.40%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                168</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+        
+        box_2.innerHTML=`<b>Crossbows Market Size is growing at CAGR of 10.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                185</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+    
+        box_3.innerHTML=`<b>Metal Packaging Coatings Market Size is growing at CAGR of 3.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                199</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+}
+
+function Chemicals(){
+    head_1.innerHTML=`<h1>Chemicals</h1>
+        <p>CAGR of 5.6% <br> Market Value of $23.9 Trillion</p>`;
+    
+        box_1.innerHTML=`<b>Disc Golf Market Size is growing at CAGR of 17.40%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                168</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+        
+        box_2.innerHTML=`<b>Crossbows Market Size is growing at CAGR of 10.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                185</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+    
+        box_3.innerHTML=`<b>Metal Packaging Coatings Market Size is growing at CAGR of 3.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                199</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+}
+
+function Electronics(){
+    head_1.innerHTML=`<h1>Electronics</h1>
+        <p>CAGR of 5.6% <br> Market Value of $23.9 Trillion</p>`;
+    
+        box_1.innerHTML=`<b>Disc Golf Market Size is growing at CAGR of 17.40%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                168</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+        
+        box_2.innerHTML=`<b>Crossbows Market Size is growing at CAGR of 10.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                185</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+    
+        box_3.innerHTML=`<b>Metal Packaging Coatings Market Size is growing at CAGR of 3.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                199</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+                // opt5.target.className = "active";
+}
+function Pharmacy(){
+    head_1.innerHTML=`<h1>Pharmacy</h1>
+        <p>CAGR of 5.6% <br> Market Value of $23.9 Trillion</p>`;
+    
+        box_1.innerHTML=`<b>Disc Golf Market Size is growing at CAGR of 17.40%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                168</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+        
+        box_2.innerHTML=`<b>Crossbows Market Size is growing at CAGR of 10.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                185</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+    
+        box_3.innerHTML=`<b>Metal Packaging Coatings Market Size is growing at CAGR of 3.30%, this report covers analysis by Type, segmentation, growth and forecast 2023-2030</b>
+        <p class="d-flex flex-md-row flex-column justify-content-between align-items-md-center align-items-start"><span>Pages :
+                199</span><span>Published: 11 days ago</span><span>Price : $ 3590</span></p>`;
+}
+function myFunction(e) {
+    var elems = document.querySelectorAll(".active");
+    [].forEach.call(elems, function(el) {
+      el.classList.remove("active");
+    });
+    e.target.className = "active";
+  }
+
